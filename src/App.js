e086476400284f7389872744b0579cbf63dd5579
dsanguti <dsanguti@gmail.com>
2024-05-06 12:30:09 +0200

    Se estructura el proyecto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import freCodeCampLogo from './img/freecodecamp-logo.svg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='freecodecamp-logo-contenedor'>
+        <h4 className='titulo-logo'> FreeCodeCamp</h4>
+        <img 
+          src= { freCodeCampLogo }
+          className='freecodecamp-logo'
+          alt='logo de freeCodeCamp' />
+      </div>
     </div>
   );
 }
